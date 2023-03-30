522fcc484729ce48cafce5f087e029b139384cab
Dan848 <daniele.ceravolo15@gmail.com>
2023-03-30 15:51:36 +0200

    uploead low laytout function script

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const { createApp } = Vue
+
+createApp({
+    data(){
+        return{
+            toDoList: [
+                {
+                    text: "pane",
+                    done: false
+                },
+            ],
+            addText: "",
+        }
+    },
+    methods: {
+        addItem(){
+            //Ci assicura
+            if (this.addText && this.addText.length > 1) {
+                const newItem = {
+                    text: this.addText,
+                    done: false
+                }
+                this.toDoList.push(newItem);
+            }
+            this.addText = "";
+        },
+        removeItem(index){
+            this.toDoList.splice(index, 1);
+        },
+        doneItem(index){
+            this.toDoList[index].done ?
+            this.toDoList[index].done = false :
+            this.toDoList[index].done = true
+        }
+    },
+    mounted(){}
+}).mount("#app")
